08d90c686f912b6bc92dd6b3d0ea3bcee52d43d5
Dave Guarino <dave@codeforamerica.org>
2016-07-31 23:54:09 -0700

    Refactor to remove some jank duplication

--- lib/five_thirty_eight_bot.rb
+++ lib/five_thirty_eight_bot.rb
@@ -37,6 +37,22 @@ Forecast Model Update
 END
   end
 
+  def polls_plus_string
+    "Polls-plus: #{forecast_for("plus","D")}% to #{forecast_for("plus","R")}%"
+  end
+
+  def polls_only_string
+    "Polls-only: #{forecast_for("polls", "D")}% to #{forecast_for("polls", "R")}%"
+  end
+
+  def now_cast_string
+    "Now-cast: #{forecast_for("now", "D")}% to #{forecast_for("now", "R")}%"
+  end
+
+  def forecast_for(model, party)
+    data_hash[party]["models"][model]["winprob"].round(1)
+  end
+
   def page_source
     @ps ||= HTTParty.get('https://projects.fivethirtyeight.com/2016-election-forecast/')
   end
@@ -45,26 +61,5 @@ END
     extracted_json = page_source.body.match(/race\.stateData = (.+)\;/).captures.first
     parsed_json = JSON.parse(extracted_json)
     @dh ||= parsed_json["forecasts"]["latest"]
-  end
-
-  def polls_plus_string
-    hillary = data_hash["D"]["models"]["plus"]["winprob"].round(1)
-    the_donald = data_hash["R"]["models"]["plus"]["winprob"].round(1)
-
-    "Polls-plus: #{hillary}% to #{the_donald}%"
-  end
-
-  def polls_only_string
-    hillary = data_hash["D"]["models"]["polls"]["winprob"].round(1)
-    the_donald = data_hash["R"]["models"]["polls"]["winprob"].round(1)
-
-    "Polls-only: #{hillary}% to #{the_donald}%"
-  end
-
-  def now_cast_string
-    hillary = data_hash["D"]["models"]["now"]["winprob"].round(1)
-    the_donald = data_hash["R"]["models"]["now"]["winprob"].round(1)
-
-    "Now-cast: #{hillary}% to #{the_donald}%"
   end
 end
